5781f4a2d166438b309149971b7d18940402ff60
Yasmin002 <79284636+Yasmin002@users.noreply.github.com>
2022-06-03 18:40:53 -0300

    Final

--- components/Cep.js
+++ components/Cep.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 
-export default function Cep() {
+export default function Cep(props) {
   return (
    <>
-   <Text style={styles.text}> Cidade:</Text>
-   <Text style={styles.text}> Estado:</Text>
-   <Text style={styles.text}> Logradouro:</Text>
-   <Text style={styles.text}> Complemento:</Text>
+   <Text style={styles.text}> Sua Cidade: {props.data.localidade}</Text>
+   <Text style={styles.text}> Bairro: {props.data.bairro}</Text>
+   <Text style={styles.text}> Estado: {props.data.uf}</Text>
+   <Text style={styles.text}> Logradouro: {props.data.logradouro}</Text>
+   <Text style={styles.text}> Complemento: {props.data.complemento}</Text>
    </>
   );
 }
